8bb1b48edd61acf48e67c0c62b50887c6fb2261e
ilya protsenko <ilya.protsenko@cyper.ca>
2022-03-28 10:35:38 +0300

    added new enviroment for firebaseConfig and create new file .env, installed dotenv, added new ignore file in the .gitignore file

--- storage/storage.js
+++ storage/storage.js
@@ -3,14 +3,14 @@ import { getStorage  } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyAgBj6wfzAPDMKHsTcrg4MYDtyPLrhQ0TU",
-   authDomain: "fileloader-42e2f.firebaseapp.com",
-   projectId: "fileloader-42e2f",
-   storageBucket: "fileloader-42e2f.appspot.com",
-   messagingSenderId: "740330206763",
-   appId: "1:740330206763:web:8e6cbf5269db288963dc6e"
+   apiKey: process.env.API_KEY,
+   authDomain: process.env.AUTH_DOMAIN,
+   projectId: process.env.PROJECT_ID,
+   storageBucket: process.env.STORAGE_BUCKET,
+   messagingSenderId:  process.env.MESSAGING_SENDER_ID,
+   appId: process.env.APP_ID
 };
- 
+
 initializeApp(firebaseConfig);
 
 
